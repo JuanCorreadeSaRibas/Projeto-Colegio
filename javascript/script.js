4c80c81feb318e91efa7022fdb62511f690ddcc8
Juan Correa de Sá Ribas <79611807+JuanCorreadeSaRibas@users.noreply.github.com>
2024-03-19 23:09:00 -0300

    Adicionando coisas no main

--- javascript/script.js
+++ javascript/script.js
@@ -2,3 +2,25 @@ document.getElementById('open_btn').addEventListener('click', function(){
     document.getElementById('sidebar').classList.toggle('open-sidebar');
 });
 
+const tabs = document.querySelectorAll('.tab-btn');
+
+tabs.forEach(tab => tab.addEventListener('click', () => tabClicked(tab)));
+
+const tabClicked = (tab) => {
+
+    tabs.forEach(tab => tab.classList.remove('active'));
+    tab.classList.add('active');
+
+
+    const contents = document.querySelectorAll('.content');
+    contents.forEach(content => content.classList.remove('show'));
+
+    const contentId = tab.getAttribute('content-id');
+    const content = document.getElementById(contentId);
+
+    content.classList.add('show');
+}
+
+const currentActiveTab = document.querySelector('.tab-btn.active');
+tabClicked(currentActiveTab);
+
